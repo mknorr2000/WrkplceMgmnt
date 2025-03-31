618001c396f32a68c86cef77e7e24d7b7e6dfa49
mknor2000 <maxknorr183@gmail.com>
2025-03-31 18:27:18 +0200

    bookingPage almost done

--- wrkplcmgmnt/src/app/booking/page.tsx
+++ wrkplcmgmnt/src/app/booking/page.tsx
@@ -1,25 +1,31 @@
-
 import React from 'react';
 import Link from 'next/link';
+import styles from './BookingPage.module.css'; // Importiere die CSS-Datei
+import Image from 'next/image';
 
 
 const BookingPage = () => {
-    return (
-        <main>
-        <h1>Welcome to Workplace - Management Web-App!</h1>
-        <p>Here u can book a  work or parking space </p>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/booking/workplace">Workplace Booking</Link>
-            </li>
-            <li>
-              <Link href="/booking/parkplace">Parkplace Booking</Link>
-            </li>
-          </ul>
-        </nav>
-      </main>
-      );
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.heading}>Welcome to Workplace - Management Web-App!</h1>
+      <p className={styles.description}>Here you can easily book a work or parking space.</p>
+      <div>
+        <img src="wrkplcmgmnt/public/workplaceSymbol.png"></img>
+      </div>
+      <div className={styles.splitContainer}>
+        <a href="/booking/workplace" className={styles.link}>
+        <div className={styles.bookingSection}>
+        <h2>Workplace Booking</h2>
+        </div>
+        </a>
+          <a href="/booking/parkplace" className={styles.link}>
+          <div className={styles.bookingSection}>
+          <h2>Parkplace Booking</h2>
+          </div>
+          </a>
+      </div>  
+    </main>
+  );
 };
 
 export default BookingPage;
